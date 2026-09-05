d2a404d2e9e63ad7dfef4aa16e612cb7af6b52c8
Tejasv Bhargava <tejasvbhargava16@gmail.com>
2026-09-06 01:55:45 +0530

    0528-SWAPPING-NODES-IN-A-LINKED-LIST Stats: Time: 2 ms (50.36%), Space: 119 MB (67.29%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.java
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.java
@@ -8,22 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode swapNodes(ListNode head, int k) {
+//         if(head.next == null) return head;
+//        ListNode slow = head;
+//        ListNode fast = head;
+//        ListNode swap = head;
+//        for(int i =1;i <k;i++){
+//         fast = fast.next;
+//        } 
+//        slow = fast;
+       
+//        while(fast.next != null){
+//          swap = swap.next;
+//          fast = fast.next;
+//        }
+  
+//        int temp = swap.val;
+//        swap.val = slow.val;
+//        slow.val = temp;
+
+//        return head;
+//     }
+// }
+
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
         if(head.next == null) return head;
        ListNode slow = head;
        ListNode fast = head;
        ListNode swap = head;
-       for(int i =1;i <k;i++){
+       for(int i =1;i <=k;i++){
         fast = fast.next;
        } 
-       slow = fast;
-       
-       while(fast.next != null){
-         swap = swap.next;
+
+       while(fast != null){
+         slow = slow.next;
          fast = fast.next;
        }
-    //    for(int i =1;i < k;i++) swap = swap.next;
+       for(int i =1;i < k;i++) swap = swap.next;
        int temp = swap.val;
        swap.val = slow.val;
        slow.val = temp;
